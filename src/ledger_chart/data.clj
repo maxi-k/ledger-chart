2b42f5f629a5a5d6ff16cb4cbddfd26c244cef66
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2016-11-15 21:30:55 +0100

    Fix walk-structure function

--- src/ledger_chart/data.clj
+++ src/ledger_chart/data.clj
@@ -51,29 +51,31 @@
   ;; fnv :: vector -> {:result (maybe result) :recur-on (maybe keys)}
   ;; fnm :: map -> {:result (maybe result) :recur-on (maybe keys)}
   ([fnm fnv s]
-   (let [handler ;; handle a result from one of the callbacks
-         (fn [item]
-           (if (nil? (item :result))
-             ;; if the result was nil return nil
-             nil
-             ;; if the result has something to recur on, do that
-             ;; otherwise just result it
-             (if-let [rec (item :recur-on)]
-               (if (vector? rec)
-                 ;; if there's multiple things to recur on, reduce over the keys,
-                 ;; associng the recursive result onto the result-map as key
-                 (reduce (fn [col k]
-                           (assoc col k (walk-structure fnv fnm (col k))))
-                         (item :result)
-                         rec)
-                 ;; Otherwise assoc the resursive result onto the result map directly
-                 (assoc (item :result) rec (walk-structure fnv fnm (-> item :result rec))))
-               (item :result))))]
+   (letfn [(handler [item] ;; handle a result from one of the callbacks
+             (if-let [item-result (and item (item :result))]
+               ;; if the result has something to recur on, do that
+               ;; otherwise just return it
+               (if-let [rec (item :recur-on)]
+                 (if (vector? rec)
+                   ;; if there's multiple things to recur on, reduce over the keys,
+                   ;; associng the recursive result onto the result-map as key
+                   (apply vector
+                          (reduce (fn [col k]
+                                    (assoc col k (walk-structure fnm fnv (col k))))
+                                  item-result
+                                  rec))
+                   ;; Otherwise assoc the resursive result onto the result map directly
+                   (assoc item-result rec (walk-structure fnm fnv (-> item :result rec))))
+                 item-result)))]
+     ;; If the structure is a vector, apply the callback to each element
+     ;; Otherwise apply the handler to the result directly
      (if (vector? s)
-       ;; If the structure is a vector, apply the callback to each element
-       (let [res (map s fnv)]
-         (map handler res))
-       ;; Otherwise apply the handler to the result directly
+       (apply vector
+              (reduce (fn [col v]
+                        (if-let [res (handler (fnv v))]
+                          (conj col res)
+                          col))
+                      [] s))
        (handler (fnm s))))))
 
 (defn get-accounts [xml]
