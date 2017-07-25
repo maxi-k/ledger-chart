c6ebccec5d92e1e12b42685e04c8ca7285540661
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2017-07-25 16:06:07 +0200

    Start with blank slate for electron based approach

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to ledger-chart
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- test/ledger_chart/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns ledger-chart.core-test
-  (:require [clojure.test :refer :all]
-            [ledger-chart.core :refer :all]))
-
-(deftest a-test
-  (testing "Dummy"
-    (is (= 1 1))))
