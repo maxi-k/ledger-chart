64dcbf3df5888fd554e5e46e2ccf52b005aa0472
Maximilian Kuschewski <maxi.kuschewski@gmail.com>
2017-07-31 14:38:37 +0200

    Only display the file name in the tile bar, not the whole path

--- src_front/ledger_chart_front/client.cljs
+++ src_front/ledger_chart_front/client.cljs
@@ -1,6 +1,7 @@
 (ns ledger-chart-front.client
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [ledger-chart-front.data :as data]
+            [clojure.string :as str]
             [cljs.core.async :as async :refer [chan <! >!]]))
 
 (def electron (js/require "electron"))
@@ -8,7 +9,8 @@
 
 (defn choose-file []
   (let [files (js->clj (.showOpenDialog (.-dialog remote) {:properties ["openFile"]}))
-        file (nth files 0)]
+        file (nth files 0)
+        file-name (last (str/split file #"/"))]
     (when (some? file)
       (reset! data/current-file file)
-      (reset! data/window-title (str file " - " (:app-title data/constants))))))
+      (reset! data/window-title (str file-name " - " (:app-title data/constants))))))
